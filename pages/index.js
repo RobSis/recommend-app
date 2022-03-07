346827cdd3ad64fff6a7dc7e46f69bb5a90aeaa0
Robert Šiška <robert.siska@magnolia-cms.com>
2022-03-07 12:41:08 +0100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { Typography, Grid, Container } from '@mui/material';
+import { TopBar, Teaser } from '../components';
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+export default function HomePage() {
+  return (
+    <>
+      <TopBar />
+      <main>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          <Typography
+            component="h1"
+            variant="h2"
+            align="left"
+            color="text.primary"
+            gutterBottom
+          >
+            Latest Reviews
+          </Typography>
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Teaser />
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+    </>
+  );
+}
