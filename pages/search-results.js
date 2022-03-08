2810a25a9c904a6747571837bc8c686ede9ea49d
Jaroslav Simak <jaroslav.simak@magnolia-cms.com>
2022-03-08 16:42:05 +0100

    added title to search results page

--- pages/search-results.js
+++ pages/search-results.js
@@ -4,6 +4,7 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import {setURLSearchParams} from "../utils";
 import SearchTeaser from "../templates/components/search/SearchTeaser";
+import {Typography} from "@mui/material";
 
 const defaultBaseUrl = process.env.NEXT_PUBLIC_MGNL_HOST;
 
@@ -26,20 +27,31 @@ export default function BasicGrid() {
     }, []);
 
     return (
-        <Box sx={{flexGrow: 1}}>
-            {results && results.length > 0 ? (
-                <Grid container spacing={2}>
-                    {results.map((item, index) => {
-                        return (
-                            <Grid item xs={12} key={index}>
-                                <SearchTeaser item={item}/>
-                            </Grid>
-                        )
-                    })}
-                </Grid>
-            ) : (
-                "Nothing to display."
-            )}
-        </Box>
+        <>
+            <Typography
+                component="h1"
+                variant="h2"
+                align="left"
+                color="text.primary"
+                gutterBottom
+            >
+                Search Results
+            </Typography>
+            <Box sx={{flexGrow: 1}}>
+                {results && results.length > 0 ? (
+                    <Grid container spacing={2}>
+                        {results.map((item, index) => {
+                            return (
+                                <Grid item xs={12} key={index}>
+                                    <SearchTeaser item={item}/>
+                                </Grid>
+                            )
+                        })}
+                    </Grid>
+                ) : (
+                    "Nothing to display."
+                )}
+            </Box>
+        </>
     );
 }
