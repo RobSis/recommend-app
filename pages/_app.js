84d0a8a62cce8dc8fbf04e2d54bcbb00238cfad4
Jaroslav Simak <jaroslav.simak@magnolia-cms.com>
2022-03-08 15:41:51 +0100

    added common layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,14 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import Head from 'next/head';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from '../src/theme';
+import Layout from "../components/layout";
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
 
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
+      <Layout>
         <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+      </Layout>
   );
 }
 
